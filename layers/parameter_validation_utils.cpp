b048460b8b62e8fd3f30819405c265b180abac3d
Mark Lobodzinski <mark@lunarg.com>
2022-12-27 17:18:22 -0700

    layers: Add support for VK_EXT_swapchain/surface_maintenance1
    These extensions allow increased flexibility and control of creating
    and managing swapchains. This patch adds support for the
    instance extension VK_EXT_surface_maintenance1 and the device
    extension VK_EXT_swapchain_maintenance1.
    
    Change-Id: Ieed25591e964dfd168b3ce87a86bc006e23c3f6b

--- layers/parameter_validation_utils.cpp
+++ layers/parameter_validation_utils.cpp
@@ -9268,6 +9268,45 @@ bool StatelessValidation::manual_PreCallValidateGetPhysicalDeviceSurfaceCapabili
         }
     }
 #endif
+
+    if (IsExtEnabled(device_extensions.vk_ext_surface_maintenance1)) {
+            const auto *surface_present_mode_compatibility =
+            LvlFindInChain<VkSurfacePresentModeCompatibilityEXT>(pSurfaceCapabilities->pNext);
+        const auto *surface_present_scaling_compatibilities =
+            LvlFindInChain<VkSurfacePresentScalingCapabilitiesEXT>(pSurfaceCapabilities->pNext);
+
+        if (surface_present_mode_compatibility && !(LvlFindInChain<VkSurfacePresentModeEXT>(pSurfaceInfo->pNext))) {
+            skip |= LogError(device, "VUID-vkGetPhysicalDeviceSurfaceCapabilities2KHR-pNext-07776",
+                             "vkGetPhysicalDeviceSurfaceCapabilities2KHR(): VK_EXT_surface_maintenance1 is enabled and "
+                             "pSurfaceCapabilities->pNext contains "
+                             "VkSurfacePresentModeCompatibilityEXT, but pSurfaceInfo->pNext does not contain a "
+                             "VkSurfacePresentModeEXT structure.");
+        }
+
+        if (surface_present_scaling_compatibilities && !(LvlFindInChain<VkSurfacePresentModeEXT>(pSurfaceInfo->pNext))) {
+            skip |= LogError(device, "VUID-vkGetPhysicalDeviceSurfaceCapabilities2KHR-pNext-07777",
+                             "vkGetPhysicalDeviceSurfaceCapabilities2KHR(): VK_EXT_surface_maintenance1 is enabled and "
+                             "pSurfaceCapabilities->pNext contains "
+                             "VkSurfacePresentScalingCapabilitiesEXT, but pSurfaceInfo->pNext does not contain a "
+                             "VkSurfacePresentModeEXT structure.");
+        }
+
+        if (IsExtEnabled(instance_extensions.vk_google_surfaceless_query)) {
+            if (surface_present_mode_compatibility && (pSurfaceInfo->surface == VK_NULL_HANDLE)) {
+                skip |= LogError(physicalDevice, "VUID-vkGetPhysicalDeviceSurfaceCapabilities2KHR-pNext-07778",
+                                 "vkGetPhysicalDeviceSurfaceCapabilities2KHR: VK_EXT_surface_maintenance1 and "
+                                 "VK_GOOGLE_surfaceless_query are enabled and pSurfaceCapabilities->pNext contains a "
+                                 "VkSurfacePresentModeCompatibilityEXT structure, but pSurfaceInfo->surface is VK_NULL_HANDLE.");
+            }
+
+            if (surface_present_scaling_compatibilities && (pSurfaceInfo->surface == VK_NULL_HANDLE)) {
+                skip |= LogError(physicalDevice, "VUID-vkGetPhysicalDeviceSurfaceCapabilities2KHR-pNext-07779",
+                                 "vkGetPhysicalDeviceSurfaceCapabilities2KHR: VK_EXT_surface_maintenance1 and "
+                                 "VK_GOOGLE_surfaceless_query are enabled and pSurfaceCapabilities->pNext contains a "
+                                 "VkSurfacePresentScalingCapabilitiesEXT structure, but pSurfaceInfo->surface is VK_NULL_HANDLE.");
+            }
+        }
+    }
     return skip;
 }
 
